dbeec4df16fa16e3eaee9c0c04db183ef240e167
Sean Aguirre <sean.aguirre@soleranetwork.com>
2018-01-03 17:39:46 -0700

    Added works directory

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fun-with-channels
+
+An example and practice with GO channels
---
+++ works/worker.go
@@ -0,0 +1,47 @@
+package works
+
+type WG struct {
+	main    chan func()
+	allDone chan bool
+}
+
+func New(n int) WG {
+	res := WG{
+		main:    make(chan func()),
+		allDone: make(chan bool),
+	}
+
+	procDone := make(chan bool)
+
+	for i := 0; i < n; i++ {
+		go func() {
+			for {
+				f := <-res.main
+				if f == nil {
+					procDone <- true
+					return
+				}
+				f()
+			}
+
+		}()
+	}
+
+	go func() {
+		for i := 0; i < n; i++ {
+			_ = <-procDone
+		}
+		res.allDone <- true
+	}()
+
+	return res
+}
+
+func (wg WG) Add(f func()) {
+	wg.main <- f
+}
+
+func (wg WG) Wait() {
+	close(wg.main)
+	_ = <-wg.allDone
+}
